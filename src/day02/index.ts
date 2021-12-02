3ca7431527f3b6bbeaaeb701e3e992c8d89118da
Andrew Snelling <72226000+snelling-a@users.noreply.github.com>
2021-12-02 09:15:49 +0100

    day02_part02

--- src/day02/index.ts
+++ src/day02/index.ts
@@ -31,7 +31,26 @@ const part1 = (rawInput: string) => {
 const part2 = (rawInput: string) => {
   const input = parseInput(rawInput);
 
-  return;
+  let horizontalPosition = 0;
+  let depth = 0;
+  let aim = 0;
+
+  for (const commandAndAmount of input) {
+    switch (commandAndAmount[0]) {
+      case "forward":
+        horizontalPosition += commandAndAmount[1];
+        depth += commandAndAmount[1] * aim;
+        break;
+      case "up":
+        aim -= commandAndAmount[1];
+        break;
+      case "down":
+        aim += commandAndAmount[1];
+      default:
+        break;
+    }
+  }
+  return horizontalPosition * depth;
 };
 
 run({
@@ -53,7 +72,17 @@ run({
   },
   part2: {
     tests: [
-      // { input: ``, expected: "" },
+      {
+        input: `
+        forward 5
+        down 5
+        forward 8
+        up 3
+        down 8
+        forward 2
+      `,
+        expected: 900,
+      },
     ],
     solution: part2,
   },
