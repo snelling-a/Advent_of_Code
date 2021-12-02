ea414759b77547910c1681fafee9e079d329af5d
Andrew Snelling <72226000+snelling-a@users.noreply.github.com>
2021-12-02 06:40:37 +0100

    day02_part01

---
+++ src/day02/index.ts
@@ -0,0 +1,61 @@
+import run from "aocrunner";
+
+const parseInput = (rawInput: string): [string, number][] =>
+  rawInput
+    .split("\n")
+    .map((line) => [line.split(" ")[0], parseInt(line.split(" ")[1])]);
+
+const part1 = (rawInput: string) => {
+  const input = parseInput(rawInput);
+  let horizontalPosition = 0;
+  let depth = 0;
+
+  for (const commandAndAmount of input) {
+    switch (commandAndAmount[0]) {
+      case "forward":
+        horizontalPosition += commandAndAmount[1];
+        break;
+      case "up":
+        depth -= commandAndAmount[1];
+        break;
+      case "down":
+        depth += commandAndAmount[1];
+      default:
+        break;
+    }
+  }
+
+  return horizontalPosition * depth;
+};
+
+const part2 = (rawInput: string) => {
+  const input = parseInput(rawInput);
+
+  return;
+};
+
+run({
+  part1: {
+    tests: [
+      {
+        input: `
+        forward 5
+        down 5
+        forward 8
+        up 3
+        down 8
+        forward 2
+      `,
+        expected: 150,
+      },
+    ],
+    solution: part1,
+  },
+  part2: {
+    tests: [
+      // { input: ``, expected: "" },
+    ],
+    solution: part2,
+  },
+  trimTestInputs: true,
+});
