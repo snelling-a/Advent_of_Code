09e25a12cee0ac66ed2ca1de94547e6d07810d23
Andrew Snelling <72226000+snelling-a@users.noreply.github.com>
2021-12-05 21:24:36 +0100

    [2015] day02_part1: tests passing, wrong answer

--- 2015/src/day02/index.ts
+++ 2015/src/day02/index.ts
@@ -1,13 +1,70 @@
 import run from "aocrunner";
 
-type Input = string[];
+class Present {
+  private readonly length: number;
+  private readonly width: number;
+  private readonly height: number;
 
-const parseInput = (rawInput: string): Input => rawInput.split("\n");
+  private readonly baseParams: number[];
+
+  constructor(length: number, width: number, height: number) {
+    this.length = length;
+    this.width = width;
+    this.height = height;
+    this.baseParams = [this.length, this.width, this.height];
+  }
+
+  public getSmallestSide(): {
+    smallestSide: number;
+    value: number;
+  } {
+    const baseParams = this.baseParams;
+    const value = Math.min(...baseParams);
+    const smallestSide = baseParams.indexOf(value);
+
+    return { value, smallestSide };
+  }
+
+  public getBaseArea() {
+    return (
+      2 * this.length * this.width +
+      2 * this.width * this.height +
+      2 * this.height * this.length
+    );
+  }
+
+  public getExtraArea() {
+    switch (this.getSmallestSide().smallestSide) {
+      case 0:
+        return this.length * this.width;
+      case 1:
+        return this.width * this.height;
+      case 2:
+        return this.height * this.length;
+      default:
+        throw new Error("Invalid smallest side");
+    }
+  }
+
+  public getWrappingPaperArea() {
+    return this.getBaseArea() + this.getExtraArea();
+  }
+}
+
+const parseInput = (rawInput: string): Present[] =>
+  rawInput.split("\n").map((present: string) => {
+    const [length, width, height] = present.split("x").map(Number);
+    return new Present(length, width, height);
+  });
 
 const part1 = (rawInput: string) => {
   const input = parseInput(rawInput);
 
-  return;
+  return input.reduce(
+    (totalWrappingPaperArea: number, currentPresent) =>
+      (totalWrappingPaperArea += currentPresent.getWrappingPaperArea()),
+    0,
+  );
 };
 
 const part2 = (rawInput: string) => {
