ebcb7e2565e1e76739970cfffa7bd7d47ea6b337
Andrew Snelling <72226000+snelling-a@users.noreply.github.com>
2021-12-06 20:54:46 +0100

    [2021] day06 (#11)
    * [2021] day06: init
    
    * [2021] day06_part1: solved
    
    * [2021] day06_part2: solved
    
    * Pre .gitignore changes
    
    * Post .gitignore changes

--- 2021/README.md
+++ 2021/README.md
@@ -16,8 +16,7 @@
 [![Day](https://badgen.net/badge/03/%E2%98%85%E2%98%85/green)](src/day03)
 [![Day](https://badgen.net/badge/04/%E2%98%85%E2%98%85/green)](src/day04)
 [![Day](https://badgen.net/badge/05/%E2%98%85%E2%98%85/green)](src/day05)
-![Day](https://badgen.net/badge/05/%E2%98%86%E2%98%86/gray)
-![Day](https://badgen.net/badge/06/%E2%98%86%E2%98%86/gray)
+[![Day](https://badgen.net/badge/06/%E2%98%85%E2%98%85/green)](src/day06)
 ![Day](https://badgen.net/badge/07/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/08/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/09/%E2%98%86%E2%98%86/gray)
@@ -71,23 +70,23 @@ Both parts: 4.13ms
 
 ```
 Day 04
-Time part 1: 27.68ms
-Time part 2: 16.95ms
-Both parts: 44.629999999999995ms
+Time part 1: 28.02ms
+Time part 2: 19.42ms
+Both parts: 47.44ms
 ```
 
 ```
 Day 05
-Time part 1: 88.73ms
-Time part 2: 156.49ms
-Both parts: 245.22000000000003ms
+Time part 1: 85.96ms
+Time part 2: 146.65ms
+Both parts: 232.61ms
 ```
 
 ```
 Day 06
-Time part 1: -
-Time part 2: -
-Both parts: -
+Time part 1: 0.06ms
+Time part 2: 0.06ms
+Both parts: 0.12ms
 ```
 
 ```
@@ -224,8 +223,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 8/50
-Total time: 252.84000000000003ms
+Total stars: 12/50
+Total time: 287.78999999999996ms
 ```
 
 <!--/RESULTS-->
---
+++ 2021/src/day06/index.ts
@@ -0,0 +1,53 @@
+import run from 'aocrunner';
+
+type Input = number[];
+
+const parseInput = (rawInput: string): Input => rawInput.split(',').map(Number);
+
+const getStartingAges = (startingLanternfish: Input) => {
+	const lanternfishAges = Array(9).fill(0);
+
+	for (let i = 0; i < startingLanternfish.length; i++) {
+		lanternfishAges[startingLanternfish[i]]++;
+	}
+
+	return lanternfishAges;
+};
+
+const modelLanternfishGrowthRate = (startingLanternfish: Input, days: number) => {
+	const lanternfishAges = getStartingAges(startingLanternfish);
+
+	for (let day = 0; day < days; day++) {
+		const dayZeroFish = lanternfishAges.shift();
+		lanternfishAges[6] += dayZeroFish;
+		lanternfishAges[8] = dayZeroFish;
+	}
+
+	return lanternfishAges.reduce((a, b) => a + b);
+};
+
+const part1 = (rawInput: string) => {
+	const startingLanternfish = parseInput(rawInput);
+
+	return modelLanternfishGrowthRate(startingLanternfish, 80);
+};
+
+const part2 = (rawInput: string) => {
+	const startingLanternfish = parseInput(rawInput);
+
+	return modelLanternfishGrowthRate(startingLanternfish, 256);
+};
+
+const testCase = '3,4,3,1,2';
+
+run({
+	part1: {
+		tests: [{ input: testCase, expected: 5934 }],
+		solution: part1,
+	},
+	part2: {
+		tests: [{ input: testCase, expected: 26984457539 }],
+		solution: part2,
+	},
+	trimTestInputs: true,
+});
