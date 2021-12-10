5e1ec28566e371bde2ac687c1b5185b86841b644
Andrew Snelling <72226000+snelling-a@users.noreply.github.com>
2021-12-10 07:07:15 +0100

    [2015] day03, part1 solved

--- 2015/README.md
+++ 2015/README.md
@@ -13,7 +13,7 @@
 
 [![Day](https://badgen.net/badge/01/%E2%98%85%E2%98%85/green)](src/day01)
 [![Day](https://badgen.net/badge/02/%E2%98%85%E2%98%85/green)](src/day02)
-![Day](https://badgen.net/badge/03/%E2%98%86%E2%98%86/gray)
+[![Day](https://badgen.net/badge/03/%E2%98%85%E2%98%86/yellow)](src/day03)
 ![Day](https://badgen.net/badge/04/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/05/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/06/%E2%98%86%E2%98%86/gray)
@@ -76,16 +76,16 @@ Both parts: 0.65ms
 
 ```
 Day 02
-Time part 1: 2.04ms
-Time part 2: 2.67ms
+Time part 1: 1.96ms
+Time part 2: 2.75ms
 Both parts: 4.71ms
 ```
 
 ```
 Day 03
-Time part 1: -
+Time part 1: 3230.878ms
 Time part 2: -
-Both parts: -
+Both parts: 3230.877583ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 4/50
-Total time: 5.359999999999999ms
+Total stars: 5/50
+Total time: 3236.238ms
 ```
 
 <!--/RESULTS-->
--- 2015/src/day03/index.ts
+++ 2015/src/day03/index.ts
@@ -4,10 +4,53 @@ type Input = string[];
 
 const parseInput = (rawInput: string): Input => rawInput.split("");
 
+const followDirections = (input: Input) => {
+  const startingPointKey = { x: 0, y: 0 };
+  let lastPointKey = startingPointKey;
+
+  const map = input.reduce((acc, direction) => {
+    let currentPointKey = lastPointKey;
+
+    switch (direction) {
+      case "^":
+        currentPointKey = { x: lastPointKey.x, y: lastPointKey.y - 1 };
+        acc.has(JSON.stringify(currentPointKey))
+          ? acc.get(JSON.stringify(currentPointKey))!.visits++
+          : acc.set(JSON.stringify(currentPointKey), { visits: 1 });
+        lastPointKey = currentPointKey;
+        return acc;
+      case "v":
+        currentPointKey = { x: lastPointKey.x, y: lastPointKey.y + 1 };
+        acc.has(JSON.stringify(currentPointKey))
+          ? acc.get(JSON.stringify(currentPointKey))!.visits++
+          : acc.set(JSON.stringify(currentPointKey), { visits: 1 });
+        lastPointKey = currentPointKey;
+        return acc;
+      case ">":
+        currentPointKey = { x: lastPointKey.x + 1, y: lastPointKey.y };
+        acc.has(JSON.stringify(currentPointKey))
+          ? acc.get(JSON.stringify(currentPointKey))!.visits++
+          : acc.set(JSON.stringify(currentPointKey), { visits: 1 });
+        lastPointKey = currentPointKey;
+        return acc;
+      case "<":
+        currentPointKey = { x: lastPointKey.x - 1, y: lastPointKey.y };
+        acc.has(JSON.stringify(currentPointKey))
+          ? acc.get(JSON.stringify(currentPointKey))!.visits++
+          : acc.set(JSON.stringify(currentPointKey), { visits: 1 });
+        lastPointKey = currentPointKey;
+        return acc;
+      default:
+        return acc;
+    }
+  }, new Map([[JSON.stringify(startingPointKey), { visits: 1 }]]));
+  return map.size;
+};
+
 const part1 = (rawInput: string) => {
   const input = parseInput(rawInput);
 
-  return;
+  return followDirections(input);
 };
 
 const part2 = (rawInput: string) => {
@@ -36,5 +79,5 @@ run({
     solution: part2,
   },
   trimTestInputs: true,
-  onlyTests: true,
+  onlyTests: false,
 });
