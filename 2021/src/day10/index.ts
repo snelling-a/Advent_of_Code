61b322d418497f9af1c5a4e33ff0adf2bc5b0a21
Andrew Snelling <72226000+snelling-a@users.noreply.github.com>
2021-12-13 09:42:43 +0100

    [2021] day10 pt1 solved

--- 2021/src/day10/index.ts
+++ 2021/src/day10/index.ts
@@ -1,28 +1,49 @@
 import run from 'aocrunner';
 
-type Input = Line[];
-type Line = SyntaxChar[];
-type SyntaxChar = keyof typeof OpeningChar | keyof typeof ClosingChar;
-enum OpeningChar {
-	'(',
-	'[',
-	'{',
-	'<',
-}
-enum ClosingChar {
-	')',
-	']',
-	'}',
-	'>',
-}
+type Input = string[][];
 
 const parseInput = (rawInput: string): Input =>
 	rawInput.split('\n').map((line) => line.split('')) as Input;
 
+const pairs = new Map<string, string>([
+	['(', ')'],
+	['[', ']'],
+	['{', '}'],
+	['<', '>'],
+]);
+
+const points = new Map<string, number>([
+	[')', 3],
+	[']', 57],
+	['}', 1197],
+	['>', 25137],
+]);
+
+const getFirstCorruptedChar = (input: Input) => {
+	let total = 0;
+	for (const line of input) {
+		const expectedClosers: string[] = [];
+
+		for (const char of line) {
+			if (pairs.has(char)) {
+				expectedClosers.push(pairs.get(char) as string);
+			} else {
+				const expected = expectedClosers.pop();
+				if (expected != char) {
+					total += points.get(char) as number;
+					break;
+				}
+			}
+		}
+	}
+
+	return total;
+};
+
 const part1 = (rawInput: string) => {
 	const input = parseInput(rawInput);
 
-	return;
+	return getFirstCorruptedChar(input);
 };
 
 const part2 = (rawInput: string) => {
