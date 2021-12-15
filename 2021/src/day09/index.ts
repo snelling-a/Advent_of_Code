7d51c75f49625be1f5edc3118553f55a062df9a5
Andrew Snelling <72226000+snelling-a@users.noreply.github.com>
2021-12-15 07:10:48 +0100

    [2021] day09 refactor for reusability

--- 2021/src/day09/index.ts
+++ 2021/src/day09/index.ts
@@ -5,29 +5,29 @@ type Coord = { row: number; col: number; risk: number };
 
 const parseInput = (rawInput: string): Input =>
 	rawInput.split('\n').map((line) => line.split('').map(Number));
+const isLowestPoint = (input: Input, rowIndex: number, colIndex: number) => {
+	const currentDepth = input[rowIndex][colIndex];
+	const depthToTheRight = input[rowIndex]?.[colIndex + 1] ?? 9;
+	const depthToTheLeft = input[rowIndex]?.[colIndex - 1] ?? 9;
+	const depthBelow = input[rowIndex + 1]?.[colIndex] ?? 9;
+	const depthAbove = input[rowIndex - 1]?.[colIndex] ?? 9;
+
+	const test =
+		currentDepth < depthToTheRight &&
+		currentDepth < depthToTheLeft &&
+		currentDepth < depthAbove &&
+		currentDepth < depthBelow;
+
+	return { currentDepth, test };
+};
 
 const getLowPoints = (input: Input): Coord[] => {
 	const lowPoints: Coord[] = [];
 	for (let rowIndex = 0; rowIndex < input.length; rowIndex++) {
 		for (let colIndex = 0; colIndex < input[rowIndex].length; colIndex++) {
-			const currentDepth = input[rowIndex][colIndex];
-			const isFirstRow = rowIndex === 0;
-			const isLastRow = rowIndex === input.length - 1;
-			const isFirstCol = colIndex === 0;
-			const isLastCol = colIndex === input[rowIndex].length - 1;
+			const { currentDepth, test } = isLowestPoint(input, rowIndex, colIndex);
 
-			const depthToTheRight = isLastCol ? 9 : input[rowIndex][colIndex + 1];
-			const depthToTheLeft = isFirstCol ? 9 : input[rowIndex][colIndex - 1];
-			const depthBelow = isLastRow ? 9 : input[rowIndex + 1][colIndex];
-			const depthAbove = isFirstRow ? 9 : input[rowIndex - 1][colIndex];
-			if (
-				currentDepth < depthToTheRight &&
-				currentDepth < depthToTheLeft &&
-				currentDepth < depthAbove &&
-				currentDepth < depthBelow
-			) {
-				lowPoints.push({ row: rowIndex, col: colIndex, risk: currentDepth + 1 });
-			}
+			test && lowPoints.push({ row: rowIndex, col: colIndex, risk: currentDepth + 1 });
 		}
 	}
 
