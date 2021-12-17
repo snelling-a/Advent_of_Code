d677e49a998ada9721ad43870bf5d9b6736dbc1c
Andrew Snelling <72226000+snelling-a@users.noreply.github.com>
2021-12-17 16:10:09 +0100

    [2021] day16 init (#22)

---
+++ 2021/src/day16/index.ts
@@ -0,0 +1,34 @@
+// https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm#Pseudocode
+
+import run from 'aocrunner';
+
+type Input = string[];
+
+const parseInput = (rawInput: string): Input => rawInput.split('\n');
+
+const part1 = (rawInput: string) => {
+	const input = parseInput(rawInput);
+
+	return;
+};
+
+const part2 = (rawInput: string) => {
+	const input = parseInput(rawInput);
+
+	return;
+};
+
+const testCase = '';
+
+run({
+	part1: {
+		tests: [{ name: '', input: testCase, expected: '' }],
+		solution: part1,
+	},
+	part2: {
+		tests: [{ name: '', input: testCase, expected: '' }],
+		solution: part2,
+	},
+	trimTestInputs: true,
+	onlyTests: true,
+});
