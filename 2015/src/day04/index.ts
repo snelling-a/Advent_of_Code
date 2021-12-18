c87455fb0f00599e56d48970466972e61e5945fc
Andrew Snelling <72226000+snelling-a@users.noreply.github.com>
2021-12-18 06:07:38 -0500

    [2015] day04, part 1: solved

---
+++ 2015/src/day04/index.ts
@@ -0,0 +1,47 @@
+import run from 'aocrunner';
+import { createHash } from 'crypto';
+
+const parseInput = (rawInput: string) => rawInput;
+
+const hash = (input: string): string =>
+    createHash('md5').update(input).digest('hex');
+
+const getLowestNonce = (input: string, leadingZeros: number): number => {
+    const prefix = '0'.repeat(leadingZeros);
+
+    let nonce = 1;
+    while (!hash(input + nonce).startsWith(prefix)) {
+        nonce++;
+    }
+
+    return nonce;
+};
+
+const part1 = (rawInput: string) => {
+    const input = parseInput(rawInput);
+
+    return getLowestNonce(input, 5);
+};
+
+const part2 = (rawInput: string) => {
+    const input = parseInput(rawInput);
+    return;
+};
+
+const testCase = '';
+
+run({
+    part1: {
+        tests: [
+            { name: '', input: `abcdef`, expected: 609043 },
+            { name: '', input: `pqrstuv`, expected: 1048970 },
+        ],
+        solution: part1,
+    },
+    part2: {
+        // tests: [{ name: '', input: testCase, expected: '' }],
+        solution: part2,
+    },
+    trimTestInputs: true,
+    // onlyTests: true,
+});
