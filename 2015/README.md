e90a5de26e9f1d8d04c247c264c22336bef60f29
Andrew Snelling <72226000+snelling-a@users.noreply.github.com>
2021-12-23 21:36:01 -0500

    [2015] day06, part 1: solved

--- 2015/README.md
+++ 2015/README.md
@@ -16,7 +16,7 @@
 [![Day](https://badgen.net/badge/03/%E2%98%85%E2%98%85/green)](src/day03)
 [![Day](https://badgen.net/badge/04/%E2%98%85%E2%98%85/green)](src/day04)
 [![Day](https://badgen.net/badge/05/%E2%98%85%E2%98%85/green)](src/day05)
-![Day](https://badgen.net/badge/06/%E2%98%86%E2%98%86/gray)
+[![Day](https://badgen.net/badge/06/%E2%98%85%E2%98%86/yellow)](src/day06)
 ![Day](https://badgen.net/badge/07/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/08/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/09/%E2%98%86%E2%98%86/gray)
@@ -97,16 +97,16 @@ Both parts: 8131.316114ms
 
 ```
 Day 05
-Time part 1: 0.688ms
-Time part 2: 0.822ms
-Both parts: 1.510253ms
+Time part 1: 0.63ms
+Time part 2: 0.709ms
+Both parts: 1.3389479999999998ms
 ```
 
 ```
 Day 06
-Time part 1: -
+Time part 1: 354.651ms
 Time part 2: -
-Both parts: -
+Both parts: 354.651452ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 10/50
-Total time: 8153.189ms
+Total stars: 11/50
+Total time: 8507.67ms
 ```
 
 <!--/RESULTS-->
---
+++ 2015/src/day06/index.ts
@@ -0,0 +1,94 @@
+import run from 'aocrunner';
+
+type Light = { row: number; column: number };
+type Range = { from: Light; to: Light };
+type Action = 'turn on' | 'turn off' | 'toggle';
+type Instruction = [Action, Range];
+type Input = Instruction[];
+
+const parseInput = (rawInput: string): Input =>
+    rawInput.split('\n').map((line) => {
+        const match = line.match(
+            /(?<action>turn on|turn off|toggle) (?<fromRow>\d+),(?<fromColumn>\d+) through (?<toRow>\d+),(?<toColumn>\d+)/,
+        );
+
+        const { action, fromRow, fromColumn, toRow, toColumn } =
+            match?.groups ?? {};
+
+        return [
+            action,
+            {
+                from: { row: Number(fromRow), column: Number(fromColumn) },
+                to: { row: Number(toRow), column: Number(toColumn) },
+            },
+        ] as Instruction;
+    });
+
+const create2DArray = <T>(rows: number, cols: number, defaultValue: T): T[][] =>
+    Array.from(Array(rows), () => Array(cols).fill(defaultValue));
+
+const lights = create2DArray(1000, 1000, false);
+
+const part1 = (rawInput: string) => {
+    const input = parseInput(rawInput);
+
+    for (const [action, { from, to }] of input) {
+        for (let row = from.row; row <= to.row; row++) {
+            for (let column = from.column; column <= to.column; column++) {
+                switch (action) {
+                    case 'turn on':
+                        lights[row][column] = true;
+                        break;
+                    case 'turn off':
+                        lights[row][column] = false;
+                        break;
+                    case 'toggle':
+                        lights[row][column] = !lights[row][column];
+                        break;
+                }
+            }
+        }
+    }
+
+    return lights.reduce((acc, row) => {
+        const lit = row.filter(Boolean).length;
+        return acc + lit;
+    }, 0);
+};
+
+const part2 = (rawInput: string) => {
+    const input = parseInput(rawInput);
+
+    return;
+};
+
+// const testCase = '';
+
+run({
+    part1: {
+        tests: [
+            {
+                name: 'every light',
+                input: `turn on 0,0 through 999,999`,
+                expected: 1000000,
+            },
+            {
+                name: 'first line of 1000 lights',
+                input: `toggle 0,0 through 999,0`,
+                expected: 1000,
+            },
+            {
+                name: 'leave off the middle 4 lights',
+                input: `turn off 499,499 through 500,500`,
+                expected: 0,
+            },
+        ],
+        solution: part1,
+    },
+    part2: {
+        // tests: [{ name: '', input: testCase, expected: '' }],
+        solution: part2,
+    },
+    trimTestInputs: true,
+    // onlyTests: true,
+});
